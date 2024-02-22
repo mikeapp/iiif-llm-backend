d4a8c52bca80de0abad5447d6f891c8675431719
Michael Appleby <michael.appleby@yale.edu>
2024-02-22 17:07:41 -0500

    Adding Titan

--- lambda_api/api_handler.py
+++ lambda_api/api_handler.py
@@ -1,11 +1,14 @@
 from database import hello_world
 from chatgpt import call_gpt
+from titan import call_titan
 import boto3
 
 def lambda_handler(event, context):
     message = hello_world()
-    completion = call_gpt("Please translate this Latin text", "nautae dant dona feminis")
+    gpt_completion = call_gpt("Please translate this Latin text", "nautae dant dona feminis")
+    titan_completion = call_titan("what is the answer?", " 2 * 2")
     return {
         'message': message,
-        'completion': completion
+        'gpt': gpt_completion,
+        'titan': titan_completion
     }
---
+++ lambda_api/titan.py
@@ -0,0 +1,65 @@
+import json
+import logging
+import boto3
+import urllib
+
+from botocore.exceptions import ClientError
+
+def generate_text(model_id, body):
+    print(
+        "Generating text with Amazon &titan-text-express; model %s", model_id)
+
+    bedrock = boto3.client(service_name='bedrock-runtime')
+
+    accept = "application/json"
+    content_type = "application/json"
+
+    response = bedrock.invoke_model(
+        body=body, modelId=model_id, accept=accept, contentType=content_type
+    )
+    response_body = json.loads(response.get("body").read())
+
+    finish_reason = response_body.get("error")
+
+    if finish_reason is not None:
+        raise Exception(f"Text generation error. Error is {finish_reason}")
+
+    print(
+        "Successfully generated text with Amazon &titan-text-express; model %s", model_id)
+
+    return response_body
+
+
+def call_titan(prompt, text):
+    response_body = []
+    try:
+        logging.basicConfig(level=logging.INFO,
+                            format="%(levelname)s: %(message)s")
+
+        model_id = 'amazon.titan-text-express-v1'
+
+        body = json.dumps({
+            "inputText": text + "\n " + prompt ,
+            "textGenerationConfig": {
+                "maxTokenCount": 4096,
+                "stopSequences": [],
+                "temperature": 0,
+                "topP": 0.1
+            }
+        })
+
+        response_body = generate_text(model_id, body)
+        print(f"Input token count: {response_body['inputTextTokenCount']}")
+
+        for result in response_body['results']:
+            print(f"Token count: {result['tokenCount']}")
+            print(f"Output text: {result['outputText']}")
+            print(f"Completion reason: {result['completionReason']}")
+
+    except ClientError as err:
+        message = err.response["Error"]["Message"]
+        print("A client error occured: " +
+              format(message))
+    else:
+        print(f"Finished generating text with the Amazon &titan-text-express; model {model_id}.")
+    return response_body
