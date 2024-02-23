20c52afd9505d1577b7769318d8d3a1199d19a20
Michael Appleby <michael.appleby@yale.edu>
2024-02-23 16:08:05 -0500

    New database calls

--- lambda_api/api_handler.py
+++ lambda_api/api_handler.py
@@ -1,16 +1,26 @@
-from database import create_user_record, get_user_by_username
+from database import (create_user_record,
+                      get_user_by_username,
+                      get_connection,
+                      insert_activity_record,
+                      get_credits_used_by_username,
+                      get_ocr,
+                      get_generic_prompt_by_key)
 from chatgpt import call_gpt
 from titan import call_titan
 import boto3
 
 def lambda_handler(event, context):
-    message = create_user_record("mikea", "michael.appleby@yale.edu", 500)
-    user = get_user_by_username('mikea')
+    conn = get_connection()
+    # message = create_user_record(conn,"mikea", "michael.appleby@yale.edu", 500)
+    insert_activity_record(conn, 'mikea', 'https://foo/bar/123', "ChatGPT", 2)
+    user = get_user_by_username(conn, 'mikea')
+    credits_used = get_credits_used_by_username(conn, 'mikea')
+
     gpt_completion = ""  # call_gpt("Please translate this Latin text", "nautae dant dona feminis")
     titan_completion = "" # call_titan("what is the answer?", " 2 * 2")
     return {
         'user': user,
-        'message': message,
+        'credits_used': credits_used,
         'gpt': gpt_completion,
         'titan': titan_completion
     }
--- lambda_api/database.py
+++ lambda_api/database.py
@@ -13,8 +13,7 @@ def get_connection():
 
 
 # Users
-def create_user_record(username, email_address, credits):
-    conn = get_connection()
+def create_user_record(conn, username, email_address, credits):
     cursor = conn.cursor()
     try:
         # Prepare SQL statement
@@ -32,32 +31,117 @@ def create_user_record(username, email_address, credits):
         cursor.close()
         conn.close()
 
-def get_user_by_username(username):
+def get_user_by_username(conn, username):
     try:
-        # Connect to the database
-        conn = get_connection();
         cursor = conn.cursor();
-
-        # Prepare SQL statement
-        sql = """SELECT * FROM users WHERE username = %s"""
-
-        # Execute SQL statement
+        sql = """SELECT username, email_address, credits  FROM users WHERE username = %s"""
         cursor.execute(sql, (username,))
-
-        # Fetch the user record
         user_record = cursor.fetchone()
-
         if user_record:
-            # Print or return the user record
-            print("User found:", user_record)
             return user_record
         else:
             print("User not found.")
-
     except Exception as e:
         print("Error:", e)
-
     finally:
         # Close the cursor and connection
         cursor.close()
-        conn.close()
+
+
+# Credits used
+def get_credits_used_by_username(conn, username):
+    try:
+        cursor = conn.cursor();
+        sql = """SELECT username, credits_used FROM user_credits_used WHERE username = %s"""
+        cursor.execute(sql, (username,))
+        credits_used = cursor.fetchone()
+        if credits_used:
+            return credits_used
+        else:
+            print("UserCreditsUsed not found.")
+    except Exception as e:
+        print("Error:", e)
+    finally:
+        # Close the cursor and connection
+        cursor.close()
+
+# Activities
+def insert_activity_record(conn, username, object_id, service_provider, credits_used):
+    try:
+        cursor = conn.cursor()
+        sql = """INSERT INTO activities (username, object_id, service_provider, credits_used) 
+                 VALUES (%s, %s, %s, %s)"""
+        cursor.execute(sql, (username, object_id, service_provider, credits_used))
+        conn.commit()
+    except Exception as e:
+        print("Error:", e)
+        conn.rollback()
+    finally:
+        # Close the cursor and connection
+        cursor.close()
+
+# OCR
+def insert_ocr_record(conn, image_id, object_id, text_engine='TEXTRACT', text_content=None):
+    try:
+        cursor = conn.cursor()
+        sql = """INSERT INTO ocr (image_id, object_id, text_engine, text_content) 
+                 VALUES (%s, %s, %s, %s)"""
+        cursor.execute(sql, (image_id, object_id, text_engine, text_content))
+        conn.commit()
+    except Exception as e:
+        print("Error:", e)
+        conn.rollback()
+    finally:
+        # Close the cursor and connection
+        cursor.close()
+
+
+def get_ocr(conn, image_id, object_id, text_engine='Textract'):
+    try:
+        cursor = conn.cursor()
+        sql = """SELECT * FROM ocr WHERE image_id = %s AND object_id = %s AND text_engine = %s"""
+        cursor.execute(sql, (image_id, object_id, text_engine))
+        record = cursor.fetchone()
+        if record:
+            return record
+        else:
+            print("Record not found.")
+    except Exception as e:
+        print("Error:", e)
+    finally:
+        # Close the cursor and connection
+        cursor.close()
+
+
+# Generic Prompt
+def get_generic_prompt_by_key(conn, prompt_key):
+    try:
+        cursor = conn.cursor()
+        sql = """SELECT * FROM generic_prompts WHERE prompt_key = %s"""
+        cursor.execute(sql, (prompt_key,))
+        prompt_record = cursor.fetchone()
+        if prompt_record:
+            return prompt_record
+        else:
+            print("Prompt not found.")
+    except Exception as e:
+        print("Error:", e)
+    finally:
+        # Close the cursor and connection
+        cursor.close()
+
+def get_generic_prompts(conn):
+    try:
+        cursor = conn.cursor()
+        sql = """SELECT * FROM generic_prompts ORDER BY prompt_label"""
+        cursor.execute(sql)
+        prompt_record = cursor.fetchone()
+        if prompt_record:
+            return prompt_record
+        else:
+            print("Prompts not found.")
+    except Exception as e:
+        print("Error:", e)
+    finally:
+        # Close the cursor and connection
+        cursor.close()
